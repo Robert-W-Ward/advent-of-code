71f46c76716f21199bb389e59b8273fc85b13ba1
Robert Ward <robertwesleyward2019@gmail.com>
2024-12-02 22:51:14 -0600

    Day two 2024 Elixir

--- 2024/elixir/day-two/day-two.ex
+++ 2024/elixir/day-two/day-two.ex
@@ -1,13 +1,6 @@
 # Check that each line/list of numbers is monotonically increasing or decreasing
 # then check if the per element increase/decrease is at least 1 and at most 3
-defmodule MonotonicityChecker do
-  def is_monotonic(list) do
-    Enum.all?(1..(length(list) - 1), fn i ->
-      list[i] <= list[i + 1] || list[i] >= list[i + 1]
-    end)
-  end
-end
-
+# Part one: A SAFE report = monotonically increasing or decreasing by at least 1 and at most 3
 {:ok, contents} = File.read("reports.txt")
 lines = String.split(contents, "\n")
 
@@ -26,7 +19,7 @@ monotonic_lists =
   cleaned
   |> Enum.filter(fn sublist ->
     Enum.chunk_every(sublist, 2, 1, :discard)
-    |> Enum.all?(fn [a, b] ->
+    |> Enum.all?(fn [_a, _b] ->
       is_increasing =
         Enum.chunk_every(sublist, 2, 1, :discard)
         |> Enum.all?(fn [a, b] -> a <= b end)
@@ -39,6 +32,62 @@ monotonic_lists =
     end)
   end)
 
-IO.puts(length(cleaned))
-IO.puts(length(monotonic_lists))
-IO.inspect(monotonic_lists, charlists: :as_lists)
+monotonic_lists_strict =
+  monotonic_lists
+  |> Enum.filter(fn sublist ->
+    Enum.chunk_every(sublist, 2, 1, :discard)
+    |> Enum.all?(fn [_a, _b] ->
+      diff_at_least_one =
+        Enum.chunk_every(sublist, 2, 1, :discard)
+        |> Enum.all?(fn [a, b] -> abs(a - b) >= 1 end)
+
+      diff_at_most_three =
+        Enum.chunk_every(sublist, 2, 1, :discard)
+        |> Enum.all?(fn [a, b] -> abs(a - b) <= 3 end)
+
+      diff_at_least_one and diff_at_most_three
+    end)
+  end)
+
+IO.puts("Part one number of 'safe' reports #{length(monotonic_lists_strict)}")
+
+# SAFE REPORT = monotoncially increasing or decreasing by at least 1 and at most 1,
+# BUT if you can remove 1 number to make it monotonic and it still is increasing or decreasing by at least 1 and at most 3 you can consider it still "SAFE
+# Same as before but you can remove a SINGLE number to make it safe
+not_safe =
+  cleaned
+  |> Enum.filter(fn sublist ->
+    if(!Enum.member?(monotonic_lists_strict, sublist)) do
+      sublist
+    end
+  end)
+
+dampened_safe =
+  not_safe
+  |> Enum.filter(fn sublist ->
+    Enum.with_index(sublist)
+    |> Enum.any?(fn {_, index} ->
+      List.delete_at(sublist, index)
+      |> (fn new_list ->
+            is_increasing =
+              Enum.chunk_every(new_list, 2, 1, :discard)
+              |> Enum.all?(fn [a, b] -> a <= b end)
+
+            is_decreasing =
+              Enum.chunk_every(new_list, 2, 1, :discard)
+              |> Enum.all?(fn [a, b] -> a >= b end)
+
+            diff_at_least_one =
+              Enum.chunk_every(new_list, 2, 1, :discard)
+              |> Enum.all?(fn [a, b] -> abs(a - b) >= 1 end)
+
+            diff_at_most_three =
+              Enum.chunk_every(new_list, 2, 1, :discard)
+              |> Enum.all?(fn [a, b] -> abs(a - b) <= 3 end)
+
+            (is_increasing or is_decreasing) and (diff_at_least_one and diff_at_most_three)
+          end).()
+    end)
+  end)
+
+IO.puts(length(dampened_safe))
