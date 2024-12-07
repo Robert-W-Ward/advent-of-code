bd90793241555c599e535c2edca465acf8c30efb
Robert Ward <robertwesleyward2019@gmail.com>
2024-12-06 23:22:08 -0600

    Day 4 2024 Elixir

--- 2024/elixir/day-4/day-four.ex
+++ 2024/elixir/day-4/day-four.ex
@@ -1,84 +1,93 @@
-# find ALL instances of 'XMAS' in the word search they can be horizontal, vertical, diagonal, written backwards, or even overlapping other words.
-
-{:ok, contents} = File.read("wordsearch.txt")
-
 defmodule XmasCounter do
-  @directions [
-    # Horizontal: right
-    {1, 0},
-    # Horizontal: left
-    {-1, 0},
-    # Vertical: down
-    {0, 1},
-    # Vertical: up
-    {0, -1},
-    # Diagonal: down-right
-    {1, 1},
-    # Diagonal: up-right
-    {1, -1},
-    # Diagonal: down-left
-    {-1, 1},
-    # Diagonal: up-left
-    {-1, -1}
-  ]
+  @straight 0
+  @right 1
+  @left -1
+  @up -1
+  @down 1
 
-  def count_xmas(input, width) do
-    grid = parse_to_grid(input, width)
-    total_count = count_all_directions(grid, "XMAS")
-    IO.puts("Total XMAS occurences: #{total_count}")
-  end
+  # Updated to include rows, cols in arguments
+  defp count_direction(puzzle, rows, cols, row, col, dir_row, dir_col, word) do
+    len = String.length(word)
 
-  defp parse_to_grid(input, width) do
-    input
-    |> String.graphemes()
-    |> Enum.chunk_every(width)
-  end
+    check_chars = fn check_chars, r, c, i ->
+      cond do
+        i == len ->
+          1
 
-  defp count_all_directions(grid, word) do
-    Enum.reduce(@directions, 0, fn {dx, dy}, acc ->
-      acc + count_in_direction(grid, word, {dx, dy})
-    end)
-  end
+        r < 0 or r >= rows or c < 0 or c >= cols ->
+          0
 
-  defp count_in_direction(grid, word, {dx, dy}) do
-    height = length(grid)
-    width = length(List.first(grid))
+        Enum.at(Enum.at(puzzle, r), c) != String.at(word, i) ->
+          0
 
-    Enum.reduce(0..(width - 1), 0, fn x, acc_x ->
-      acc_x + Enum.reduce(0..(height - 1), 0, fn y, acc_y ->
-        acc_y + match_word_at(grid, word, x, y, {dx, dy})
-      end)
-    end)
-  end
-
-  defp match_word_at(grid, word, x, y, {dx, dy}) do
-    word_length = String.length(word)
-
-    Enum.reduce_while(0..(word_length - 1), "", fn i, acc ->
-      new_x = x + i * dx
-      new_y = y + i * dy
-
-      if valid_position?(grid, new_x, new_y) do
-        letter = Enum.at(Enum.at(grid, new_y), new_x)
-        {:cont, acc <> letter}
-      else
-        {:halt, ""}
+        true ->
+          check_chars.(check_chars, r + dir_row, c + dir_col, i + 1)
       end
-    end)
-    |> (fn result ->
-          if result == word or result == String.reverse(word) do
-            1
-          else
-            0
-          end
-        end).()
+    end
+
+    check_chars.(check_chars, row, col, 0)
   end
 
-  defp valid_position?(grid, x, y) do
-    y >= 0 and y < length(grid) and
-      x >= 0 and x < length(Enum.at(grid, y, []))
+  def part1() do
+    puzzle =
+      File.read!("wordsearch.txt")
+      |> String.split("\n", trim: true)
+      # now puzzle is a list of list of chars
+      |> Enum.map(&String.graphemes/1)
+
+    rows = length(puzzle)
+    # since puzzle is now list of graphemes
+    cols = length(hd(puzzle))
+
+    result =
+      Enum.reduce(0..(rows - 1), 0, fn i, acc ->
+        Enum.reduce(0..(cols - 1), acc, fn j, inner_acc ->
+          inner_acc +
+            count_direction(puzzle, rows, cols, i, j, @right, @straight, "XMAS") +
+            count_direction(puzzle, rows, cols, i, j, @left, @straight, "XMAS") +
+            count_direction(puzzle, rows, cols, i, j, @straight, @up, "XMAS") +
+            count_direction(puzzle, rows, cols, i, j, @straight, @down, "XMAS") +
+            count_direction(puzzle, rows, cols, i, j, @right, @down, "XMAS") +
+            count_direction(puzzle, rows, cols, i, j, @right, @up, "XMAS") +
+            count_direction(puzzle, rows, cols, i, j, @left, @down, "XMAS") +
+            count_direction(puzzle, rows, cols, i, j, @left, @up, "XMAS")
+        end)
+      end)
+
+    IO.inspect(result, label: "Part 1 Result")
+  end
+
+  defp count_cross(puzzle, rows, cols, row, col) do
+    result =
+      count_direction(puzzle, rows, cols, row + @left, col + @up, @right, @down, "MAS") +
+        count_direction(puzzle, rows, cols, row + @left, col + @down, @right, @up, "MAS") +
+        count_direction(puzzle, rows, cols, row + @right, col + @up, @left, @down, "MAS") +
+        count_direction(puzzle, rows, cols, row + @right, col + @down, @left, @up, "MAS")
+
+    if result == 2, do: 1, else: 0
+  end
+
+  def part2() do
+    puzzle =
+      File.read!("wordsearch.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.graphemes/1)
+
+    rows = length(puzzle)
+
+    cols = length(hd(puzzle))
+
+    result =
+      Enum.reduce(0..(rows - 1), 0, fn i, acc ->
+        Enum.reduce(0..(cols - 1), acc, fn j, inner_acc ->
+          inner_acc +
+            count_cross(puzzle, rows, cols, i, j)
+        end)
+      end)
+
+    IO.inspect(result, label: "Part 2 Result")
   end
 end
 
-{:ok, contents} = File.read("wordsearch.txt")
-XmasCounter.count_xmas(contents, 140)
+XmasCounter.part1()
+XmasCounter.part2()
